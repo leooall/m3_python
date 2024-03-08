c51d7d5bb0caf9e87227b72e204001feb3d3b291
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-03-07 21:44:41 -0300

    se agrega archivo mi primer programa

---
+++ dia1/mi_primer_programa.py
@@ -0,0 +1,12 @@
+nombre = "leonardo "
+apellido1 = "llaupe "
+apellido2 = "paillama "
+#edad = 31
+#python no permite sumar letras con numeros
+#print(nombre +  apellido1 + apellido2 + edad)
+#si cambiamos la edad de int a str se puede sumar
+edad = 31.5 
+#suma de string es concatenar
+print(nombre +  apellido1 + apellido2, edad, "mes", 3, "dia", 2)
+#se puede imprimir str con int, separados con una coma
+
