ee0e15eb612c4f5b797cff8df2440952a1868120
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-03-11 17:03:19 -0300

    add archivo emprendedor1

---
+++ dia3/emprendedor1.py
@@ -0,0 +1,11 @@
+import math
+p = float(input("Ingrese precio de suscripcion: \n"))
+u = float(input("Ingrese numero de usuarios: \n"))
+gt = float(input("Ingrese gastos totales: \n"))
+
+#utilidades = p * u - gt
+
+utilidades = p * u - gt
+
+print("La utilidad es: ", round(utilidades, ), "Clp")
+
