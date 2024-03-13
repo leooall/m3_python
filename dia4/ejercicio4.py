7b8cfec8f882a9662b4c8e90d9d930c081601caf
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-03-13 20:08:13 -0300

    se agregan carpetas y archivos dia4 y dia5

---
+++ dia4/ejercicio4.py
@@ -0,0 +1,40 @@
+"""
+solicitar al usuario el ingreso de 3 numeros y determinar cuales de ellos es mayor que 33 
+solo numeros enteros del 1 al 100
+y determinar cual es mayor y cual es menor 
+
+1. Analizar el problema
+
+2. Descomponer el problema en partes
+
+- se capturara 3 datos numericos del usuario solicitando ingresar solo numeros enteros del 1 al 100
+  el usuario debe ingresar el primer numero
+  validar si cumple con los requisitos anteriores
+  si cumple se continua con el ingreso del segundo numero 
+  de lo contrario arrojar un mensaje de error y solicitar el reingreso de un numero valido
+  repetir el proceso de validacion con los siguientes numeros
+  
+- realizar el proceso para determinar si el primer numero ingresado es mayor que 33 
+  luego repetir el proceso con el segundo y tercer numero ingresado 
+  si alguno cumple este requisito imprimir      
+  
+  el tercer proceso determinar cual es mayor y cual es menor
+  ............
+
+
+
+
+3. Resolver el problema
+
+"""
+"""
+calcular el promedio de 3 notas con 2 decimales ingresadas por el usuario
+
+Inicio
+Leer nota1
+Leer nota2
+Leer nota3
+(nota1+nota2+nota3)/3
+Mostrar ((nota1+nota2+nota3)/3 con 2 decimales)
+Fin
+"""
