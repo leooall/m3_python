6bedc3cdcdfb7336fdea82756ba49541521f72ab
Leonardo Llaupe <leonardollaupe@gmail.com>
2024-03-14 22:27:26 -0300

    se agregan archivos del desafio2

---
+++ desafio2/cachipun.py
@@ -0,0 +1,46 @@
+#se importa modulo random
+import random 
+
+#entrada de datos
+jugada = input("Ingresa piedra, papel o tijera\n")
+
+#generacion de la variable opciones que contendra las opciones de jugadas para el computador
+opciones = ["piedra", "papel", "tijera"]
+
+#creacion de la variable jugada_computador que contiene las opciones de jugadas de forma aleatoria gracias al modulo random.choice()
+jugada_computador = random.choice(opciones)
+
+#condicionales con sus respectivas salidas de datos
+#jugadas con piedra
+if jugada == "piedra" and jugada_computador == "piedra" :
+    print("Tu jugaste Piedra \nComputador jugó Piedra \nEmpate!!")
+    
+elif jugada == "piedra" and jugada_computador == "papel" :
+    print("Tu jugaste Piedra \nComputador jugó Papel \nPerdiste =(") 
+
+elif jugada == "piedra" and jugada_computador == "tijera" :
+    print("Tu jugaste Piedra \nComputador jugó Tijera \nGanaste!!")
+    
+#jugadas con papel
+elif jugada == "papel" and jugada_computador == "papel" :
+    print("Tu jugaste Papel \nComputador jugó Papel \nEmpate!!")
+    
+elif jugada == "papel" and jugada_computador == "tijera" :
+    print("Tu jugaste Papel \nComputador jugó Tijera \nPerdiste =(")    
+    
+elif jugada == "papel" and jugada_computador == "piedra" :
+    print("Tu jugaste Papel \nComputador jugó Piedra \nGanaste!!")    
+    
+#jugadas con tijera
+elif jugada == "tijera" and jugada_computador == "tijera" :
+    print("Tu jugaste Tijera \nComputador jugó Tijera \nEmpate!!")
+    
+elif jugada == "tijera" and jugada_computador == "papel" :
+    print("Tu jugaste Tijera \nComputador jugó Papel \nGanaste!!")
+    
+elif jugada == "tijera" and jugada_computador == "piedra" :
+    print("Tu jugaste Tijera \nComputador jugó Piedra \nPerdiste =(")
+
+
+
+
---
+++ desafio2/imc.py
@@ -0,0 +1,25 @@
+#entrada de datos
+peso = int(input("Ingresar peso en Kilogramos: \n"))
+talla = int(input("Ingresar talla en centimetros: \n"))
+
+#conversion de cm a mts como lo pide la formula
+talla = talla / 100
+
+#aplicacion de la formula imc=peso/talla2
+imc = round(peso / (talla * talla),2)
+
+#condicionales con sus respectivas salidas de datos
+if imc < 18.5 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Bajo Peso")
+elif imc >= 18.5 and imc < 25 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Adecuado")
+elif imc >= 25 and imc < 30 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Sobrepeso")
+elif imc >= 30 and imc < 35 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Obesidad Grado I")
+elif imc >= 35 and imc < 40 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Obesidad Grado II")
+elif imc > 40 :
+    print(f"Su IMC es {imc}\nLa clasificacion OMS es Obesidad Grado III")
+
+
